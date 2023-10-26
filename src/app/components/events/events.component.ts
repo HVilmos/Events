cc7ad044b5b5d600d1feccf8320a176bd44fa61c
Horváth Vilmos <horvathvilmos@Horvath-MacBook-Pro.local>
2023-10-26 21:11:30 +0200

    cards

--- src/app/components/events/events.component.ts
+++ src/app/components/events/events.component.ts
@@ -3,6 +3,11 @@ import { map } from 'rxjs';
 import { BaseService } from 'src/app/service/base.service';
 import { ConfigService } from 'src/app/service/config.service';
 
+interface MyEvents {
+  date?: string;
+  dateFormatted?: string;
+}
+
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
@@ -10,71 +15,38 @@ import { ConfigService } from 'src/app/service/config.service';
 })
 export class EventsComponent {
   events: any;
-  sortedEvents: any; 
-  searchText: string = '';
-  selectedSortOption: string = 'default'; 
+ 
 
   constructor(private base: BaseService, private config: ConfigService) {
     this.base.getData().snapshotChanges().pipe(
       map((changes) =>
-        changes.map((c) => ({ key: c.payload.key, ...c.payload.val() }))
+        changes.map((c) => {
+          const eventData: MyEvents | null = c.payload.val() as MyEvents;
+          if (eventData) {
+            const eventDate = eventData.date ? new Date(eventData.date) : null;
+            eventData.dateFormatted = eventDate ? this.formatDate(eventDate) : '';
+          }
+          return { key: c.payload.key, ...eventData };
+        })
       )
     ).subscribe(adatok => {
       this.events = adatok;
-      this.sortEvents(); 
     });
   }
 
-  sortEvents() {
-    switch (this.selectedSortOption) {
-      case 'dateAsc':
-        this.sortedEvents = this.events.slice().sort((a:any, b:any) => {
-          if (a.date && b.date) {
-            const dateA = new Date(a.date);
-            const dateB = new Date(b.date);
-            return dateA.getTime() - dateB.getTime();
-          }
-          return 0;
-        });
-        break;
-      case 'dateDesc':
-        this.sortedEvents = this.events.slice().sort((a:any, b:any) => {
-          if (a.date && b.date) {
-            const dateA = new Date(a.date);
-            const dateB = new Date(b.date);
-            return dateB.getTime() - dateA.getTime();
-          }
-          return 0;
-        });
-        break;
-      case 'priceAsc':
-        this.sortedEvents = this.events.slice().sort((a:any, b:any) => a.price - b.price);
-        break;
-      case 'priceDesc':
-        this.sortedEvents = this.events.slice().sort((a:any, b:any) => b.price - a.price);
-        break;
-      default:
-        this.sortedEvents = this.events;
-        break;
+  private formatDate(date: Date | null): string {
+    if (date) {
+      const options: Intl.DateTimeFormatOptions = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+      };
+      
+      return date.toLocaleDateString('hu-HU', options);
     }
+    return '';
   }
 
-  filterEvents() {
-    if (this.searchText) {
-      this.sortedEvents = this.events.filter((event: any) => {
-        return event.name.toLowerCase().includes(this.searchText.toLowerCase()) ||
-               event.description.toLowerCase().includes(this.searchText.toLowerCase());
-      });
-    } else {
-      this.sortEvents();
-    }
-  }
-
-  calculateRemainingDays(eventDate: string): number {
-    const currentDate = new Date();
-    const targetDate = new Date(eventDate);
-    const timeDifference = targetDate.getTime() - currentDate.getTime();
-    const daysRemaining = Math.floor(timeDifference / (1000 * 3600 * 24));
-    return daysRemaining;
-  }
+  
 }
